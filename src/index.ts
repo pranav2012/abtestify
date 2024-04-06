f91d7fa3ddf8a93a1e738b567f8cdbd83ce82b4f
pranav2012 <p2012agarwal@gmail.com>
2024-04-07 02:15:11 +0530

    chore: added linting and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,60 @@
-import crypto from 'node:crypto';
+import * as crypto from 'crypto';
 import { IExperimentData, IParams } from '@types';
 
-export const getABExperimentBucket = ({ experimentName, uniqueId, treatmentSplit = 50, exposure = 100, treatmentVariantName = 'treatment', controlVariantName = 'control', ignoreVariantName = 'ignore' }: IParams): IExperimentData => {
+export const getABExperimentBucket = ({
+	experimentName,
+	uniqueId,
+	treatmentSplit = 50,
+	exposure = 100,
+	treatmentVariantName = 'treatment',
+	controlVariantName = 'control',
+	ignoreVariantName = 'ignore',
+}: IParams): IExperimentData => {
+	if (treatmentSplit < 0 || treatmentSplit > 100) {
+		throw new Error(
+			'treatmentSplit must be a number between 0 and 100 as it represents the percentage of users who will be bucketed into the treatment group.',
+		);
+	}
+	if (exposure < 0 || exposure > 100) {
+		throw new Error(
+			'exposure must be a number between 0 and 100 as it represents the percentage of users who will be bucketed into the experiment.',
+		);
+	}
 
-    if (treatmentSplit < 0 || treatmentSplit > 100) {
-        throw new Error('treatmentSplit must be a number between 0 and 100 as it represents the percentage of users who will be bucketed into the treatment group.');
-    }
-    if (exposure < 0 || exposure > 100) {
-        throw new Error('exposure must be a number between 0 and 100 as it represents the percentage of users who will be bucketed into the experiment.');
-    }
+	try {
+		const digest = crypto.createHash('sha256');
+		const hashBytes = digest.update(uniqueId).digest(); // Calculate the hash of the ID
+		const hashedId = BigInt(
+			`0x${Buffer.from(hashBytes).subarray(0, 8).toString('hex')}`,
+		); // Convert the hashed ID to a 64-bit BigInt using subarray instead of slice
+		const normalizedHashedId =
+			Number(hashedId) / Number(BigInt('0xFFFFFFFFFFFFFFFF')); // Convert the hashed ID to a float between 0 and 1
+		const randomNum = Math.floor(normalizedHashedId * 10000); // Multiply the normalized hashed ID by 10,000 to get a random number between 0 and 9,999
 
-    try {
-        const digest = crypto.createHash('sha256');
-        const hashBytes = digest.update(uniqueId).digest(); // Calculate the hash of the ID
-        const hashedId = BigInt(`0x${Buffer.from(hashBytes).subarray(0, 8).toString('hex')}`); // Convert the hashed ID to a 64-bit BigInt using subarray instead of slice
-        const normalizedHashedId = Number(hashedId) / Number(BigInt("0xFFFFFFFFFFFFFFFF")); // Convert the hashed ID to a float between 0 and 1
-        const randomNum = Math.floor(normalizedHashedId * 10000); // Multiply the normalized hashed ID by 10,000 to get a random number between 0 and 9,999
+		if (randomNum / 100 >= exposure)
+			return {
+				experimentName,
+				uniqueId,
+				resolvedVariant: ignoreVariantName,
+			}; // Check if user should be ignored based on exposure rate
 
-        if (randomNum / 100 >= exposure) return {
-            experimentName,
-            uniqueId,
-            resolvedVariant: ignoreVariantName,
-        }; // Check if user should be ignored based on exposure rate
+		const resolvedVariant =
+			randomNum % 100 < treatmentSplit
+				? treatmentVariantName
+				: controlVariantName;
 
-        const resolvedVariant = randomNum % 100 < treatmentSplit ? treatmentVariantName : controlVariantName;
-
-        return {
-            experimentName,
-            uniqueId,
-            resolvedVariant,
-        };
-    } catch (error) {
-        return {
-            experimentName,
-            uniqueId,
-            resolvedVariant: null,
-            isError: true,
-            error,
-        };
-    }
-}
+		return {
+			experimentName,
+			uniqueId,
+			resolvedVariant,
+		};
+	} catch (error) {
+		return {
+			experimentName,
+			uniqueId,
+			resolvedVariant: null,
+			isError: true,
+			error,
+		};
+	}
+};
--- src/types/interface.ts
+++ src/types/interface.ts
@@ -2,22 +2,22 @@
  * Interface for the parameters required by the getABExperimentBucket function.
  */
 export interface IParams {
-    experimentName: string; // The name of the experiment
-    uniqueId: string; // The unique identifier for the user can be email, user ID, etc.
-    treatmentSplit?: number; // The percentage of users to be bucketed into the treatment group (default: 50)
-    exposure?: number; // The percentage of users to be bucketed into the experiment (default: 100)
-    controlVariantName?: string; // The name of the control variant (default: 'control')
-    treatmentVariantName?: string; // The name of the treatment variant (default: 'treatment')
-    ignoreVariantName?: string; // The name of the variant to be ignored (default: 'ignore')
+	experimentName: string; // The name of the experiment
+	uniqueId: string; // The unique identifier for the user can be email, user ID, etc.
+	treatmentSplit?: number; // The percentage of users to be bucketed into the treatment group (default: 50)
+	exposure?: number; // The percentage of users to be bucketed into the experiment (default: 100)
+	controlVariantName?: string; // The name of the control variant (default: 'control')
+	treatmentVariantName?: string; // The name of the treatment variant (default: 'treatment')
+	ignoreVariantName?: string; // The name of the variant to be ignored (default: 'ignore')
 }
 
 /**
  * Interface for the experiment data returned by the getABExperimentBucket function.
  */
 export interface IExperimentData {
-    experimentName: string; // The name of the experiment
-    uniqueId: string; // The unique identifier for the user
-    resolvedVariant: string | null; // The variant that the user is assigned to, or null if there was an error
-    isError?: boolean; // Indicates if there was an error while assigning the variant
-    error?: unknown; // The error object, if isError is true
+	experimentName: string; // The name of the experiment
+	uniqueId: string; // The unique identifier for the user
+	resolvedVariant: string | null; // The variant that the user is assigned to, or null if there was an error
+	isError?: boolean; // Indicates if there was an error while assigning the variant
+	error?: unknown; // The error object, if isError is true
 }
